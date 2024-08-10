9c6b03ed9898933b6a83f61db24540d8332ffbca
Femzy1900 <adedokunfemi14@gmail.com>
2024-08-10 09:16:02 -0500

    Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,10 +4,16 @@ import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom"
 import SummaryApi from "../common"
 import { toast } from "react-toastify"
+import Context from "../context";
 
 
 
 const Login = () => {
+    const [data, setData] = useState({
+        email: "",
+        password: ""
+    })
+    const [showPassword, se]
     return (
         <div className="">Login</div>
     )
