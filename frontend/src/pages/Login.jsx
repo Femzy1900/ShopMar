fcf8613e14fb1417974f0ed5ebad3878dc8cbc5e
Femzy1900 <adedokunfemi14@gmail.com>
2024-08-10 09:25:20 -0500

    connect the Login to the API

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,45 @@ const Login = () => {
         email: "",
         password: ""
     })
-    const [showPassword, se]
+    const [showPassword, setShowPassword] = useState(false)
+    const [fetchUserDetails, fetchUserAddToCart] = useContext(Context)
+    console.log("fetchUserDetails", fetchUserDetails)
+    const navigate = useNavigate()
+
+    const handleOnChange = (e) => {
+        const { name, value } = e.target
+        setData((prev) => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const dataResponse = await fetch(SummaryApi.signIn.url, {
+            method: SummaryApi.signIn.method,
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+
+        const dataApi = await dataResponse.json()
+
+        if (dataApi.success) {
+            toast.success(dataApi.message)
+            navigate("/")
+            fetchUserDetails();
+            fetchUserAddToCart()
+        }
+
+        if (dataApi.error) {
+            toast.error(dataApi.message)
+        }
+    }
     return (
         <div className="">Login</div>
     )
