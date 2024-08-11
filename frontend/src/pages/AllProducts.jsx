90c5b46b698c427c15d16557cb46a97e1ad31868
Femzy1900 <adedokunfemi14@gmail.com>
2024-08-11 11:35:42 -0500

    product upload

--- frontend/src/pages/AllProducts.jsx
+++ frontend/src/pages/AllProducts.jsx
@@ -1,8 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import SummaryApi from '../common'
+// import UploadProduct from "../components/UploadProduct";
+import AdminProductCard from '../component/AdminProductCard';
+import UploadProduct from '../component/UploadProduct';
+
 
 
 const AllProducts = () => {
+    const [openUploadProduct,setOpenUploadProduct] = useState(false)
+    const [allProduct,setAllProduct] = useState([])
+
+    const fetchAllProduct = async () => {
+        const response = await fetch(SummaryApi.allProduct.url)
+        const dataResponse = await response.json()
+
+        console.log("product data", dataResponse)
+
+        setAllProduct(dataResponse?.data || [])
+    }
+
+
     return (
-        <div className="">AllProducts</div>
+        <div className="">
+            <div className="bg-white py-2 px-4 flex justify-between items-center">
+                <h2 className='font-bold text-lg'>All Product</h2>
+                <button className='border-2 border-red-600 text-red-600 hover:bg-red-600 hover:text-white transition-all py-1 px-3 rounded-full' onClick={() => setOpenUploadProduct(true)}>Upload Product</button>
+            </div>
+
+
+            {/* all product */}
+            <div className="flex items-center flex-wrap gap-5 py-4 h-[calc(100vh-190px)] overflow-y-scroll">
+                {
+                    allProduct.map((product, index) => {
+                        <AdminProductCard data={product} key={index+"allProduct"} fetchdata={fetchAllProduct}/>
+                    })
+                }
+            </div>
+            {
+                openUploadProduct && (
+                    <UploadProduct onClose={()=>setOpenUploadProduct(false)} fetchData={fetchAllProduct}/>
+                )
+            }
+        </div>
     )
 }
 
